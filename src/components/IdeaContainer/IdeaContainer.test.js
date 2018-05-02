c25091e82ce51c90800e20a382419bb1274416e5
Christie Lynam <christielynam@gmail.com>
2018-05-01 20:42:29 -0600

    Add IdeaContainer coverage

--- src/components/IdeaContainer/IdeaContainer.test.js
+++ src/components/IdeaContainer/IdeaContainer.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IdeaContainer from '../IdeaContainer';
+import Card from '../Card';
 import { shallow } from 'enzyme';
 
 describe('IdeaContainer', () => {
+  let wrapper;
+  let mockIdeas;
+  let removeIdeaMock;
+
+  beforeEach(() => {
+    removeIdeaMock = jest.fn()
+    mockIdeas = [{title: 'hey', body: 'yall', id: 3}, {title: 'war', body: 'eagle', id: 5}]
+    wrapper = shallow(<IdeaContainer ideas={mockIdeas} removeIdea={removeIdeaMock} />)
+  })
 
   it('matches the snapshot', () => {
-    const removeIdeaMock = jest.fn()
-    const wrapper = shallow(<IdeaContainer ideas={[]} removeIdea={removeIdeaMock} />)
-
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders all the ideas', () => {
+    expect(wrapper.find(Card).length).toEqual(2)
   })
 })
