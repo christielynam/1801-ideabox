066db272b98d2770c30b6a3d0ebb3617d7fd0cca
Christie Lynam <christielynam@gmail.com>
2018-05-01 20:16:12 -0600

    Add App tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -11,29 +11,60 @@ describe('App', () => {
     wrapper = shallow(<App />)
   })
 
-  it('renders', () => {
-    expect(wrapper).toHaveLength(1)
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
   })
 
+  // this test is not necessary... this is being tested with prop types
   it('renders an IdeaContainer with the correct props', () => {
-    //const mockRemoveIdea = jest.fn()
-    //wrapper.instance().removeIdea = mockRemoveIdea
     expect(wrapper.find(IdeaContainer).prop('ideas')).toEqual([])
     expect(wrapper.find(IdeaContainer).prop('removeIdea')).toEqual(wrapper.instance().removeIdea)
   })
 
-  it('matches the snapshot', () => {
-    expect(wrapper).toMatchSnapshot()
-  })
-  describe('removeIdea', () => {
-    it('changes state', () => {
-      const initialState = [{title: 'hey', body: 'yall', id: 1}, {title: 'war', body: 'eagle', id: 2}]
-      const expected = [{title: 'hey', body: 'yall', id: 1}]
-      wrapper.setState({ ideas: initialState })
+  it('addIdea adds an idea to state', () => {
+    const initialState = []
+    const mockIdea = {title: 'hey', body: 'yall'}
+    const expected = [{title: 'hey', body: 'yall', id: 5}]
+    Date.now = jest.fn().mockImplementation(() => 5)
 
-      wrapper.instance().removeIdea(2)
+    wrapper.setState({ ideas: initialState })
 
-      expect(wrapper.state('ideas')).toEqual(expected)
-    })
+    wrapper.instance().addIdea(mockIdea)
+
+    expect(wrapper.state('ideas')).toEqual(expected)
+    expect(wrapper.state('ideas').length).toEqual(1)
   })
+
+  // it('does not add a new idea if addIdea is not passed the correct params', () => {
+  //   const initialState = []
+  //   const invalidIdea = {title: 'react is rad'}
+  //
+  //   wrapper.setState({ ideas: initialState })
+  //
+  //   wrapper.instance().addIdea(invalidIdea)
+  //
+  //   expect(wrapper.state('ideas')).toEqual(initialState)
+  // })
+
+  it('removeIdea removes an idea from state', () => {
+    const initialState = [{title: 'hey', body: 'yall', id: 1}, {title: 'war', body: 'eagle', id: 2}]
+    const expected = [{title: 'hey', body: 'yall', id: 1}]
+
+    wrapper.setState({ ideas: initialState })
+
+    wrapper.instance().removeIdea(2)
+
+    expect(wrapper.state('ideas')).toEqual(expected)
+  })
+
+  it('does not remove an idea if an id is not passsed to removeIdea', ()  => {
+    const initialState = [{title: 'hey', body: 'yall', id: 1}, {title: 'war', body: 'eagle', id: 2}]
+
+    wrapper.setState({ ideas: initialState })
+
+    wrapper.instance().removeIdea()
+
+    expect(wrapper.state('ideas')).toEqual(initialState)
+  })
+
 })
